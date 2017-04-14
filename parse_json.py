1ee3c00757a9408b7987c361396fb63fdd3d9240
mickey <mickey@mickeys-MacBook-Pro.local>
2017-04-15 01:38:09 +0200

    added json time sorting, basic tab interface implemented

--- parse_json.py
+++ parse_json.py
@@ -2,7 +2,9 @@ import json
 import urllib
 import pprint
 import locale
+
 from datetime import datetime
+from dateutil import parser
 
 pp = pprint.PrettyPrinter(indent=4)
 locale.setlocale(locale.LC_ALL, 'no_NO')
@@ -14,11 +16,14 @@ class JsonDataHandler:
         data = json.loads(response.read())
         return data
 
-    def sort_json_by_date(self,json_data):
+    def sort_json_by_date(self,json_data):        
         sorted_json = sorted(
             json_data, 
-            key=lambda item: datetime.strptime(item['date'], '%d %B %Y'), 
-            reverse=True)
+            key=lambda item: (
+                datetime.strptime(item['date'], '%d %B %Y'),
+                datetime.strptime(item['time'], '%H:%M')), 
+            reverse=True)        
+                        
         return sorted_json
 
     def sorted_json(self):
@@ -26,8 +31,8 @@ class JsonDataHandler:
         json_sorted_by_date = self.sort_json_by_date(raw_json_data)
         return json_sorted_by_date
 
-# tmp = JsonDataHandler()
-# foo = tmp.sorted_json()
+tmp = JsonDataHandler()
+foo = tmp.sorted_json()
 
-# for i in foo:
-#     print i['date']
+for i in foo:
+     print i['date'] + ' ' + i['time']
